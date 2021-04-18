17a4f1c930f69e0c211e068820ed8ceb6f39d2e0
john million <jpmillion@comcast.net>
2021-04-17 23:24:48 -0400

    deleted unused code

--- src/actions/fetchListings.js
+++ src/actions/fetchListings.js
@@ -10,12 +10,4 @@ export const fetchListings = () => {
         .then(listings => dispatch({type: 'ADD_LISTINGS', listings: listings.results}))
         .catch(e => window.alert(e.message))
     }
-//   try {
-//     dispatch({ type: 'LOADING_LISTINGS'});
-//     const resp = await fetchJsonp(`https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&api_key=${KEY}`)
-//     const listings = await resp.json();
-//     dispatch({type: 'ADD_LISTINGS', listings: listings.results})
-//   } catch(e) {
-//     window.alert(e.message);
-//   }
 }
