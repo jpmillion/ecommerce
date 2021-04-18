531efb5cf7f58de956346325ab180e05a095dd6c
john million <jpmillion@comcast.net>
2021-04-17 20:56:50 -0400

    created Listings Container

---
+++ src/ListingComponents/Container/ListingsContainer.js
@@ -0,0 +1,42 @@
+
+import './App.css';
+import React, { Component } from 'react';
+import fetchJsonp from 'fetch-jsonp';
+import Listings from '../Presentational/Listings';
+
+
+const KEY = process.env.REACT_APP_ETSY_API_KEY;
+
+class ListingsContainer extends Component {
+
+  state = {
+    listings: []
+  }
+
+  fetchEtsy = async () => {
+    try {
+      const resp = await fetchJsonp(`https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&api_key=${KEY}`)
+      const listings = await resp.json();
+      this.setState({
+        listings: listings.results
+      })
+    } catch(e) {
+      window.alert(e.message);
+    }
+  }
+
+  componentDidMount() {
+    this.fetchEtsy();
+  }
+
+  render() {
+    
+    return (
+      <div>
+        <Listings products={this.state.listings}/>
+      </div>
+    );
+  }
+}
+
+export default ListingsContainer;
