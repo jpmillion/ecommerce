23906e918cbe907a766f57fb331ff43b57abcb62
john million <jpmillion@comcast.net>
2021-04-17 21:37:51 -0400

    moving files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 //import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-import ListingsContainer from './ListingsComponent/ListingsContainer';
+import ListingsContainer from './ListingsContainer';
 
 
 class App extends Component {
