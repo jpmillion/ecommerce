bd361e7af4c30ef26c86b142aa77d672854c7405
john million <jpmillion@comcast.net>
2021-04-19 01:22:58 -0400

    deleted debugger in login reducer

--- src/reducers/logInReducer.js
+++ src/reducers/logInReducer.js
@@ -2,7 +2,6 @@ const logInReducer = (state = {customer: {}, loggedIn: false}, action) => {
     console.log('logInReducer')
     switch (action.type) {
         case 'LOG_IN':
-            debugger
             return {
                 ...state,
                 customer: action.customer,
