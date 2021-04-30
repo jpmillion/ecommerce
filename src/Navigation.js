b4cd99c87ffa6c5355d03e5a9d9c66651d0240a8
john million <jpmillion@comcast.net>
2021-04-30 14:52:57 -0400

    changed Home from class to function, connect in Navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import NavBar from 'react-bootstrap/NavBar';
-//import Button from 'react-bootstrap/Button';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 const Navigation = props => {
-    if (props.loggedIn) {
+
+    const logOut = () => {
+        sessionStorage.clear();
+        props.dispatch({type: 'LOG_OUT'})
+    }
+
+    if (props.logIn) {
         return (
             <NavBar className='bg-secondary justify-content-center' style={{padding: '10px', fontSize: '2em'}} >
-                <NavLink to='/' onClick={props.logOut} className='text-light'>Log Out</NavLink>
+                <NavLink to='/' onClick={logOut} className='text-light'>Log Out</NavLink>
             </NavBar>
         )
     }
     return (
         <div className='row bg-secondary text-center' style={{padding: '10px', fontSize: '2em'}}> 
-            <div className="col-sm-3"></div>
-            <div className="col-sm-2"><NavLink className='text-light' to='/listings'>Listings</NavLink></div>
+            <div className="col-sm-4"></div>
             <div className="col-sm-2"><NavLink className='text-light' to='/login'>Log In</NavLink></div>
             <div className="col-sm-2"><NavLink className='text-light' to='/register'>Register</NavLink></div>
-            <div className="col-sm-3"></div>
+            <div className="col-sm-4"></div>
         </div>
     )
 }
 
-export default Navigation
+const mapStateToProps = state => {
+    return {
+        logIn: state.logIn
+    }
+}
+
+export default connect(mapStateToProps)(Navigation)
+
+
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,34 +1,17 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import CustomerContainer from '../customer/CustomerContainer';
 import { displayCart } from '../helpers';
 import Navigation from '../Navigation';
 
-export class Home extends PureComponent {
-
-    logOut = () => {
-        sessionStorage.clear();
-        this.props.dispatch({type: 'LOG_OUT'})
-    }
-
-    render() {
-        return (
-            <div>
-                <Navigation loggedIn={this.props.loggedIn} logOut={this.logOut} />
-                <div>{displayCart()}</div>
-                <CustomerContainer />
-            </div>
-        )
-    }
+export const Home = () => {
+    return (
+        <div>
+            <Navigation />
+            <div>{displayCart()}</div>
+            <CustomerContainer />
+        </div>
+    )
 }
 
-const mapStateToProps = state => {
-    return {
-        loggedIn: state.logIn,
-        cartItems: state.cartItems,
-        firstName: state.customer.first_name,
-        cartTotal: state.cart.total
-    }
-}
 
-export default connect(mapStateToProps)(Home);
+export default Home;
