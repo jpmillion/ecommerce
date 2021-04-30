80ba8429b6b80a874833c87f58b0d47b11886f5f
john million <jpmillion@comcast.net>
2021-04-30 15:12:02 -0400

    refractored trending listings

--- src/ListingComponents/Presentational/Listings.js
+++ src/ListingComponents/Presentational/Listings.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchCreateCartItem } from '../../actions/fetchCreateCartItem';
 import Listing from './Listing';
 
-const Listings = (props) => {
+const Listings = props => {
 
-    const listings = props.products.map((product) => (<Listing key={product.listing_id} { ...product } addToCart={props.addToCart} cartId={props.cartId}/>))
-
+    const listings = props.products.map((product) => (<Listing key={product.listing_id} { ...product } addToCart={props.fetchCreateCartItem} cartId={props.cartId}/>))
     return (
         <div>
             {listings}
@@ -12,4 +13,10 @@ const Listings = (props) => {
     )
 }
 
-export default Listings
+const mapStateToProps = state => {
+    return {
+        cartId: state.cart.id
+    }
+}
+
+export default connect(mapStateToProps, { fetchCreateCartItem })(Listings)
--- src/TrendingListingsContainer.js
+++ src/TrendingListingsContainer.js
@@ -1,36 +1,28 @@
 import './App.css';
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { fetchTrendingListings } from './actions/fetchTrendingListings';
 import { fetchCreateCartItem } from './actions/fetchCreateCartItem';
 import Listings from './listingComponents/Presentational/Listings';
 import { NavLink } from 'react-router-dom';
 import { displayCart } from './helpers';
 
-class ListingsContainer extends PureComponent {
-
-  render() {
+const ListingsContainer = props => {
     
     return (
       <div>
         <NavLink to='/'>Home</NavLink>
         {displayCart()}
-        <Listings products={this.props.trendingListings} addToCart={this.props.fetchCreateCartItem} cartId={this.props.cartId}/>
+        <Listings products={props.trendingListings} />
       </div>
     );
-  }
 }
 
 const mapStateToProps = state => {
   return {
     trendingListings: state.trendingListings,
     loading: state.loading,
-    loggedIn: state.logIn,
-    cartItems: state.cartItems,
-    firstName: state.customer.first_name,
     cartId: state.cart.id,
-    cartTotal: state.cart.total
   }
 }
 
-export default connect(mapStateToProps, { fetchTrendingListings, fetchCreateCartItem })(ListingsContainer);
+export default connect(mapStateToProps, { fetchCreateCartItem })(ListingsContainer);
