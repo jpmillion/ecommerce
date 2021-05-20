9ba33f6647ce62fe29bd86db16ef49a2da8b8147
john million <jpmillion@comcast.net>
2021-05-19 20:11:57 -0400

    Updates

--- src/helpers.js
+++
@@ -1,9 +0,0 @@
-import Cart from "./customer/Cart"
-
-export const displayNumOfCartItems = cartItems => {
-    return cartItems.reduce((t, {quantity}) => {return t + quantity}, 0)
-}
-
-export const displayCart = () => {
-    if (sessionStorage.token) return <Cart />
-}
