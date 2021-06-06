19a6f20d02a976243fab2df93b5422b1fb991c10
john million <jpmillion@comcast.net>
2021-06-05 21:00:50 -0400

    working on different way to filter listings

--- src/ListingComponents/ListingsContainer.js
+++ src/ListingComponents/ListingsContainer.js
@@ -7,21 +7,32 @@ import { displayCart } from '../helpers';
 const ListingsContainer = props => {
 
   const [text, setText] = useState('');
-
+  const words = text.split(' ');
+  if (words[words.length-1] === '') {
+    words.pop();
+  }
 
     const handleChange = (e) => {
-      setText(e.target.value);
+      setText(e.target.value.toLowerCase());
     }
-
-    const filteredListings = props.listings.filter(listing => listing.title.includes(text));
-
+    console.log(words)
+    const filteredListings = props.listings.filter(listing => listing.title.toLowerCase().includes(text));
+    const f = [];
+    for (const l of props.listings) {
+      for (const w of words) {
+        if (w === '') continue;
+        if (l.title.toLowerCase().includes(w)) {
+          if (!f.includes(l)) f.push(l);
+        }
+      }
+    }
 
     return (
       <div>
         <NavLink to='/'>Home</NavLink><br></br>
         <input type='text' onChange={(e) => handleChange(e)} name='text'></input>
         {/* <Listings products={props.listings}/> */}
-        <Listings products={filteredListings} />
+        <Listings products={f.length ? f : props.listings} />
         {displayCart()}
       </div>
     );
