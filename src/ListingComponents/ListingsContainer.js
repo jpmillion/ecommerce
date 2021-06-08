06730b9e04d3357438c1338e4c11374ddb10cb9e
john million <jpmillion@comcast.net>
2021-06-08 19:23:31 -0400

    altered listing container to use filtered listings

--- src/ListingComponents/ListingsContainer.js
+++ src/ListingComponents/ListingsContainer.js
@@ -1,28 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import Listings from './Listings';
-import { NavLink } from 'react-router-dom';
-import { displayCart } from '../helpers';
+import FilteredListings from './FilteredListings';
 
 const ListingsContainer = props => {
 
-  const [text, setText] = useState('');
-
-  const words = text.split(' ');
-
-  const handleChange = (e) => {
-    setText(e.target.value.toLowerCase());
-  }
-  
-  const filteredListings = props.listings.filter(listing => words.every(word => listing.title.toLowerCase().includes(word)));
-
   return (
-    <div>
-      <NavLink to='/ecommerce'>Home</NavLink><br></br>
-      <input type='text' onChange={(e) => handleChange(e)} name='text'></input>
-      <Listings products={filteredListings} />
-      {displayCart()}
-    </div>
+    <FilteredListings filteredListings={props.listings} />
   );
 }
 
