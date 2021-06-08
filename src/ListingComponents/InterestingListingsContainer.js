9c041eb51142b23975c76fab60e3035eeba33e81
john million <jpmillion@comcast.net>
2021-06-08 19:27:26 -0400

    returning filtered listing component in Interesting listings

--- src/ListingComponents/InterestingListingsContainer.js
+++ src/ListingComponents/InterestingListingsContainer.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Listings from './Listings';
-import { NavLink } from 'react-router-dom';
-import { displayCart } from '../helpers';
+import FilteredListings from './FilteredListings';
 
 const InterestingListingsContainer = props => {
     
     return (
-      <div>
-        <NavLink to='/ecommerce'>Home</NavLink>
-        <Listings products={props.listings}/>
-        {displayCart()}
-      </div>
+      <FilteredListings filteredListings={props.listings} />
     );
 }
 
