c75c2c44632f38341587f953c0cbdc55fee99fe0
john million <jpmillion@comcast.net>
2021-06-20 18:44:03 -0400

    body instead of text in card

--- src/customer/Order.js
+++ src/customer/Order.js
@@ -4,10 +4,10 @@ import Card from 'react-bootstrap/esm/Card';
 const Order = props => {
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Text>
+            <Card.Body>
                 {props.created_at}<br></br>
                 {props.total}
-            </Card.Text>
+            </Card.Body>
         </Card>
     )
 }
